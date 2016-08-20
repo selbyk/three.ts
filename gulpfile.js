9bb1ef49dae9b32a051f1f52126c8ddb1b76d8a9
Selby Kendrick <selby.kendrick@gmail.com>
2016-08-19 20:48:03 -0700

    es6ify gulpfile, other trivial changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,24 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
-var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
-var paths = {
+'use strict';
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const tsify = require('tsify');
+const gutil = require('gulp-util');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
+const webserver = require('gulp-webserver');
+const awspublish = require('gulp-awspublish');
+const parallelize = require('concurrent-transform');
+const del = require('del');
+
+let paths = {
     pages: ['src/*.html', 'src/*.css', 'src/*.png']
 };
-var webserver = require('gulp-webserver');
-var awspublish = require('gulp-awspublish');
-var parallelize = require('concurrent-transform');
-var del = require('del');
-
-/**
- * Publish files to S3
- */
-gulp.task('publish', ['bundle'], function() {
-    // create a new publisher using S3 options
-    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
-    var publisher = awspublish.create({
-        region: 'us-west-2',
-        params: {
-            Bucket: 'three-ts.selby.io'
-        },
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    });
-
-    return gulp.src('./dist/*')
-        // process in parallelize with concurrency 10
-        .pipe(parallelize(publisher.publish(), 10))
-        // sync instead of just posting
-        .pipe(publisher.sync())
-        // print upload updates to console
-        .pipe(awspublish.reporter());
-});
 
 /**
  * Start webserver with live reload
  */
-gulp.task('webserver', function() {
+gulp.task('webserver', () => {
     gulp.src('dist')
         .pipe(webserver({
             livereload: true,
@@ -51,29 +29,15 @@ gulp.task('webserver', function() {
 /**
  * Copy asset files
  */
-gulp.task('copy-assets', function() {
+gulp.task('copy-assets', () => {
     return gulp.src(paths.pages)
         .pipe(gulp.dest('dist'));
 });
 
 /**
- * Build and bundle TypeScript, then remove all generated JavaScript files from TypeScript compilation.
- */
-gulp.task('build-ts', ['bundle'], function(cb) {
-    var typeScriptGenFiles = [
-        'src/**/*.js', // path to all JS files auto gen'd by editor
-        'src/**/*.js.map', // path to all sourcemap files auto gen'd by editor
-        '!src/lib'
-    ];
-
-    // Clean up leftover js files
-    return del(typeScriptGenFiles, cb);
-});
-
-/**
  * Bundle JavaScript files generated from TypeScript compilation.
  */
-gulp.task('bundle', function() {
+gulp.task('bundle', () => {
     return browserify({
             basedir: '.',
             debug: true,
@@ -93,21 +57,54 @@ gulp.task('bundle', function() {
 });
 
 /**
- * Watch source files for changes to update dist files
+ * Build and bundle TypeScript, then remove all generated JavaScript files from TypeScript compilation.
  */
-function watch() {
+gulp.task('build', ['bundle', 'copy-assets'], (cb) => {
+    let typeScriptGenFiles = [
+        'src/**/*.js', // path to all JS files auto gen'd by editor
+        'src/**/*.js.map', // path to all sourcemap files auto gen'd by editor
+        '!src/lib'
+    ];
+
+    // Clean up leftover js files
+    return del(typeScriptGenFiles, cb);
+});
+
+/**
+ * Publish files to S3
+ */
+gulp.task('publish', ['build'], () => {
+    // create a new publisher using S3 options
+    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+    let publisher = awspublish.create({
+        region: 'us-west-2',
+        params: {
+            Bucket: 'three-ts.selby.io'
+        },
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    });
+
+    return gulp.src('./dist/*')
+        // process in parallelize with concurrency 10
+        .pipe(parallelize(publisher.publish(), 10))
+        // sync instead of just posting
+        .pipe(publisher.sync())
+        // print upload updates to console
+        .pipe(awspublish.reporter());
+});
+
+
+gulp.task('default', ['copy-assets', 'webserver', 'build'], () => {
     // Watch our assets
-    var assetWatcher = gulp.watch(paths.pages, ['copy-assets']);
+    let assetWatcher = gulp.watch(paths.pages, ['copy-assets']);
     assetWatcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
     // Watch our ts
-    var tsWatcher = gulp.watch('src/**/*.ts', ['build-ts']);
+    let tsWatcher = gulp.watch('src/**/*.ts', ['build']);
     tsWatcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
-}
-
-gulp.task('build', ['copy-assets', 'build-ts']);
-gulp.task('default', ['copy-assets', 'webserver', 'build-ts'], watch);
+});
